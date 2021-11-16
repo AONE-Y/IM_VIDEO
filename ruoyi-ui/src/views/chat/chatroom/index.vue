c1a73de1c9216c67c61cffdc6e2441b589ad9886
AONE-Y <1033825262@qq.com>
2021-11-16 16:45:23 +0800

    准备开工！！！！！！

---
+++ ruoyi-ui/src/views/chat/chatroom/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div >
+  chatroom
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ruoyi-ui/src/views/chat/videochat/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  videochat
+</div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
